bbf4326445c1e3949be3e2d616d8899422e782f4
ushasri2645 <u.gudikandula@everest.engineering>
2024-11-17 14:57:17 +0530

    Add permissions file

---
+++ src/components/Permissions.ts
@@ -0,0 +1,51 @@
+import {Alert, Platform} from 'react-native';
+import {check, PERMISSIONS, request, RESULTS} from 'react-native-permissions';
+
+export const requestPhotoLibraryPermission = async () => {
+  try {
+    if (Platform.OS === 'ios') {
+      const permissionStatus = await check(PERMISSIONS.IOS.PHOTO_LIBRARY);
+      if (permissionStatus === RESULTS.DENIED || permissionStatus === RESULTS.LIMITED) {
+        const requestStatus = await request(PERMISSIONS.IOS.PHOTO_LIBRARY);
+        if (requestStatus !== RESULTS.GRANTED) {
+          Alert.alert('Permission Denied', 'Photo Library access is required.');
+          return false;
+        }
+      }
+    } else if(Platform.OS==="android"){
+      const permissionStatus = await check(PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE);
+      if (permissionStatus === RESULTS.DENIED) {
+        const requestStatus = await request(PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE);
+        if (requestStatus !== RESULTS.GRANTED) {
+          Alert.alert('Permission Denied', 'Photo Library access is required.');
+          return false;
+        }
+      }
+    }
+    return true; 
+  } catch (e) {
+    console.error('Error checking Photo Library permission:', e);
+    Alert.alert('Error', 'An error occurred while checking Photo Library permission.');
+    return false;
+  }
+};
+
+export const requestCallPermission = async () => {
+  try {
+    if (Platform.OS === 'android') {
+      const permissionStatus = await check(PERMISSIONS.ANDROID.CALL_PHONE);
+      if (permissionStatus === RESULTS.DENIED) {
+        const requestStatus = await request(PERMISSIONS.ANDROID.CALL_PHONE);
+        if (requestStatus !== RESULTS.GRANTED) {
+          Alert.alert('Permission Denied', 'Call access is required.');
+          return false;
+        }
+      }
+    }
+    return true;
+  } catch (e) {
+    console.error('Error checking Call permission:', e);
+    Alert.alert('Error', 'An error occurred while checking Call permission.');
+    return false;
+  }
+};
