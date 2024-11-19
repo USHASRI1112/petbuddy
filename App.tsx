1bbaac994702031af0ceba8a9b28a57f9d332e9e
ushasri2645 <u.gudikandula@everest.engineering>
2024-11-19 19:32:43 +0530

    Update App with notification code

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {AppNavigator} from './Navigation';
 import {UserContextProvider} from './src/Context/Context';
+import { fetchAndScheduleReminders, setupNotificationChannel } from './src/components/Notifications';
 
 function App(): React.JSX.Element {
+  useEffect(()=>{
+    setupNotificationChannel(),
+    fetchAndScheduleReminders();
+  },[])
   return (
     <NavigationContainer>
       <UserContextProvider>
