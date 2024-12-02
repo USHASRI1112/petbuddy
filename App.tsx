0fbebdc1ca76f330e4d43c99c89a297f03f67c9e
ushasri2645 <ugudikandula@everest.engineering>
2024-12-02 11:53:17 +0530

    Update code : comment setup channel function

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {AppNavigator} from './Navigation';
 import {UserContextProvider} from './src/Context/Context';
-import { fetchAndScheduleReminders, setupNotificationChannel } from './src/components/Notifications';
+import {fetchAndScheduleReminders, setupNotificationChannel } from './src/components/Notifications';
 
 function App(): React.JSX.Element {
   useEffect(()=>{
-    setupNotificationChannel(),
+    // setupNotificationChannel(),
     fetchAndScheduleReminders();
   },[])
   return (
