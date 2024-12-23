656fe2a743c78590cd945d464dcac414ed5b6d62
ushasri2645 <ugudikandula@everest.engineering>
2024-12-23 12:33:22 +0530

    Refactor TrainingTab: separated CSS and reorganized imports

--- src/tabs/Training/TrainingTab.tsx
+++ src/tabs/Training/TrainingTab.tsx
@@ -1,13 +1,13 @@
-import React, {useEffect, useLayoutEffect} from 'react';
+import React from 'react';
 import {
-  View,
-  Text,
-  StyleSheet,
   Image,
-  ScrollView,
-  TouchableOpacity,
   Linking,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
 } from 'react-native';
+import {styles} from './TrainingTab.styles';
 
 const Training = () => {
   const openLink = (url: string) => {
@@ -85,63 +85,5 @@ const Training = () => {
     </ScrollView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFFFFF',
-    padding: 10,
-  },
-  heroSection: {
-    alignItems: 'center',
-    marginBottom: 20,
-  },
-  heroImage: {
-    width: '100%',
-    height: 200,
-    borderRadius: 10,
-  },
-  heroTitle: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: 'green',
-    marginTop: 10,
-  },
-  heroSubtitle: {
-    fontSize: 16,
-    color: 'gray',
-  },
-  section: {
-    marginBottom: 20,
-    padding: 15,
-    backgroundColor: '#E0F2E9',
-    borderRadius: 10,
-    flexDirection: 'column',
-  },
-  sectionTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: 'black',
-  },
-  sectionDescription: {
-    fontSize: 16,
-    color: 'gray',
-    marginTop: 5,
-  },
-  linkButton: {
-    marginTop: 10,
-    backgroundColor: 'forestgreen',
-    paddingVertical: 8,
-    borderRadius: 5,
-    width: '80%',
-    alignSelf: 'center',
-  },
-  linkText: {
-    color: 'white',
-    textAlign: 'center',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-});
 
 export default Training;
