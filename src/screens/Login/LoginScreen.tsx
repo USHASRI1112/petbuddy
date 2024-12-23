b9e6d71548e884c4ae123206f22f858c6125635f
ushasri2645 <ugudikandula@everest.engineering>
2024-12-23 12:30:34 +0530

    Refactored LoginScreen: extracted CSS and reordered imports

--- src/screens/Login/LoginScreen.tsx
+++ src/screens/Login/LoginScreen.tsx
@@ -1,16 +1,9 @@
-import React, {useContext, useState} from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  Image,
-  TextInput,
-  TouchableOpacity,
-  Alert,
-} from 'react-native';
-import {API_URL} from '../../../API';
-import {UserContext} from '../../Context/Context';
+import React, { useContext, useState } from 'react';
+import { Alert, Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { API_URL } from '../../../API';
+import { UserContext } from '../../Context/Context';
+import { styles } from './LoginScreen.styles';
 
 const Login = ({navigation}: {navigation: any}) => {
   const userContext = useContext(UserContext);
@@ -109,93 +102,5 @@ const Login = ({navigation}: {navigation: any}) => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    gap: 20,
-    backgroundColor: 'white',
-  },
-  topImage: {
-    resizeMode: 'stretch',
-    height: 100,
-    width: 400,
-  },
-  pawImg: {
-    borderRadius: 50,
-    height: 90,
-    width: 90,
-  },
-  middleSection: {
-    flexDirection: 'column',
-    justifyContent: 'space-evenly',
-    gap: 40,
-  },
-  petText: {
-    color: 'forestgreen',
-    fontWeight: 'bold',
-    fontSize: 25,
-  },
-  buddyText: {
-    fontWeight: 'bold',
-    fontSize: 25,
-    color: 'black',
-  },
-  petBuddyTitle: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  imageAndTitle: {
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    gap: 10,
-  },
-  inputElement: {
-    backgroundColor: 'lightgray',
-    padding: 10,
-    marginHorizontal: 60,
-    borderRadius: 10,
-  },
-  inputSection: {
-    flexDirection: 'column',
-    justifyContent: 'center',
-    gap: 20,
-  },
-  loginButton: {
-    backgroundColor: 'forestgreen',
-    padding: 10,
-    marginHorizontal: 80,
-    borderRadius: 10,
-  },
-  loginSection: {
-    flexDirection: 'column',
-    justifyContent: 'center',
-    gap: 10,
-  },
-  loginText: {
-    color: 'white',
-    textAlign: 'center',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  noAccountText: {
-    color: 'green',
-    textAlign: 'center',
-  },
-  bottomSection: {
-    flex: 1,
-    justifyContent: 'flex-end',
-  },
-  rightsText: {
-    color: 'white',
-    backgroundColor: 'green',
-    paddingVertical: 20,
-    width: 500,
-    textAlign: 'center',
-    alignSelf: 'center',
-  },
-});
 
 export default Login;
