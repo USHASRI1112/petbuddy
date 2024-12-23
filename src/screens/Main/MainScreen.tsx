a04c094fb1b172d702fbf4ebfc796e0bbc979c3f
ushasri2645 <ugudikandula@everest.engineering>
2024-12-23 12:30:46 +0530

    Refactored MainScreen: separated CSS and reorganized imports

--- src/screens/Main/MainScreen.tsx
+++ src/screens/Main/MainScreen.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import {
-  Text,
-  StyleSheet,
   ImageBackground,
+  Text,
   TouchableOpacity,
-  Platform,
   View,
 } from 'react-native';
+import { styles } from './MainScreen.styles';
 
 const Main = ({navigation}:{navigation:any}) => {
   return (
@@ -23,52 +22,5 @@ const Main = ({navigation}:{navigation:any}) => {
     </ImageBackground>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent:'flex-end'
-  },
-  bottomContainer: {
-    flexDirection: 'column',
-    alignItems:'center',
-    gap:30,
-    backgroundColor: 'rgba(255, 255, 255, 0.7)',
-    borderTopLeftRadius:20,
-    borderTopRightRadius:20,
-    padding:80
-  },
-  welcomeHeading:{
-    color:"black",
-    fontWeight:'bold',
-    fontSize:25
-  },
-  description:{
-    color:'gray',
-    fontWeight:'bold',
-    fontSize:18,
-    textAlign:'center',
-  },
-  getStartedButton:{
-    backgroundColor:'forestgreen',
-    borderRadius:10,
-    padding:10,
-    width:250,
-    flexDirection:'row',
-    paddingHorizontal:10
-  },
-  getStartedText:{
-    flex:1,
-    color:'white',
-    fontWeight:'bold',
-    textAlign:'center'
-  },
-  getStartedText1:{
-    flexDirection:'row',
-    justifyContent:'flex-end',
-    color:'white',
-    fontWeight:'bold'
-  }
-});
 
 export default Main;
