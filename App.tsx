18ddeeab6b5751e20da57088fe0ce96a40872dc4
ushasri2645 <ugudikandula@everest.engineering>
2024-12-23 11:56:55 +0530

    Move import statements in accordance with alphabetical order

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {AppNavigator} from './Navigation';
 import {UserContextProvider} from './src/Context/Context';
-import {fetchAndScheduleReminders, setupNotificationChannel } from './src/components/Notifications';
+import {fetchAndScheduleReminders } from './src/components/Notifications';
 
 function App(): React.JSX.Element {
   useEffect(()=>{
