56f1a81eee15f8b9f1e917d2deb1ec36b581b461
ushasri2645 <ugudikandula@everest.engineering>
2024-12-23 11:58:17 +0530

    Change import statements and convert from default to named import

--- src/components/AddPetModal.tsx
+++ src/components/AddPetModal.tsx
@@ -1,21 +1,29 @@
 import React, {useState} from 'react';
 import {
-  View,
-  Text,
-  StyleSheet,
-  Modal,
+  Alert,
   Image,
-  TouchableWithoutFeedback,
+  Modal,
+  StyleSheet,
+  Text,
   TextInput,
   TouchableOpacity,
-  Alert,
+  TouchableWithoutFeedback,
+  View,
 } from 'react-native';
-import { API_URL } from '../../API';
-import { requestPhotoLibraryPermission } from './Permissions';
 import ImageCropPicker from 'react-native-image-crop-picker';
 import RNFS from 'react-native-fs';
+import {API_URL} from '../../API';
+import {requestPhotoLibraryPermission} from './Permissions';
 
-const AddPetModal = ({visible, closeFn,username}: {visible: boolean; closeFn: any,username:string}) => {
+const AddPetModal = ({
+  visible,
+  closeFn,
+  username,
+}: {
+  visible: boolean;
+  closeFn: any;
+  username: string;
+}) => {
   const [name, setName] = useState('');
   const [breed, setBreed] = useState('');
   const [age, setAge] = useState('');
@@ -24,9 +32,8 @@ const AddPetModal = ({visible, closeFn,username}: {visible: boolean; closeFn: an
   const [color, setColor] = useState('');
   const [gender, setGender] = useState('');
   const [remarks, setRemarks] = useState('');
-  const [photo,setPhoto] = useState('');
-  const [emergencyContact,setEmergencyContact] = useState('')
- 
+  const [photo, setPhoto] = useState('');
+  const [emergencyContact, setEmergencyContact] = useState('');
 
   const handleImage = async () => {
     try {
@@ -54,28 +61,36 @@ const AddPetModal = ({visible, closeFn,username}: {visible: boolean; closeFn: an
     } catch (Error: any) {}
   };
 
-  const addPet = async() => {
-    try{
-        const petDetails = {
-            name,breed,gender,age,weight,height,color,remarks,image_uri:photo,emergencyContact
-        }
-        const response = await fetch(`${API_URL}pets/${username}`,{
-            method:'POST',
-            headers: {
-                'Content-Type': 'application/json',
-              },
-            body: JSON.stringify(petDetails),
-        })
-        if (response.status === 404) {
-            Alert.alert("User Not Found")
-        }
-        if(response.status===201){
-            Alert.alert("Pet added succesfully");
-            closeFn();
-        }
-    }
-    catch(e){
-        Alert.alert(`${e}`)
+  const addPet = async () => {
+    try {
+      const petDetails = {
+        name,
+        breed,
+        gender,
+        age,
+        weight,
+        height,
+        color,
+        remarks,
+        image_uri: photo,
+        emergencyContact,
+      };
+      const response = await fetch(`${API_URL}pets/${username}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(petDetails),
+      });
+      if (response.status === 404) {
+        Alert.alert('User Not Found');
+      }
+      if (response.status === 201) {
+        Alert.alert('Pet added succesfully');
+        closeFn();
+      }
+    } catch (e) {
+      Alert.alert(`${e}`);
     }
     setName('');
     setAge('');
@@ -86,23 +101,33 @@ const AddPetModal = ({visible, closeFn,username}: {visible: boolean; closeFn: an
     setBreed('');
     setRemarks('');
     setEmergencyContact('');
-  }
-  
+  };
+
   return (
     <Modal
       animationType="slide"
       visible={visible}
       transparent={true}
       onRequestClose={closeFn}>
-      <TouchableWithoutFeedback   onPress={closeFn}>
+      <TouchableWithoutFeedback onPress={closeFn}>
         <View style={styles.container} testID="pet-modal">
           <Text style={styles.header}>Enter Pet Details</Text>
-          <TouchableOpacity style={styles.imageContainer} onPress={()=>handleImage()}>
-            {!photo?
-            <Image  testID ="image-select"
-              style={styles.profileImage}
-              source={require('./../../public/assets/Register/profile.png')}
-            />:<Image testID ="image-select" style={styles.profileImage} source={{uri:photo}} />}
+          <TouchableOpacity
+            style={styles.imageContainer}
+            onPress={() => handleImage()}>
+            {!photo ? (
+              <Image
+                testID="image-select"
+                style={styles.profileImage}
+                source={require('./../../public/assets/Register/profile.png')}
+              />
+            ) : (
+              <Image
+                testID="image-select"
+                style={styles.profileImage}
+                source={{uri: photo}}
+              />
+            )}
             <Text>Upload Profile Pic</Text>
           </TouchableOpacity>
           <View style={styles.bottomContainer}>
@@ -192,7 +217,10 @@ const AddPetModal = ({visible, closeFn,username}: {visible: boolean; closeFn: an
               />
             </View>
           </View>
-          <TouchableOpacity testID='add-pet' onPress={()=>addPet()} style={styles.submit}>
+          <TouchableOpacity
+            testID="add-pet"
+            onPress={() => addPet()}
+            style={styles.submit}>
             <Text style={styles.addPet}>Add pet</Text>
           </TouchableOpacity>
         </View>
@@ -280,4 +308,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default AddPetModal;
+export {AddPetModal};
