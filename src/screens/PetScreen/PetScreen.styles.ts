8a74732e4120b5ea037f8477a89fc5e56cb11dd2
ushasri2645 <ugudikandula@everest.engineering>
2025-01-12 19:45:14 +0530

    Adjust margin bottom to 10 for pet screen

--- src/screens/PetScreen/PetScreen.styles.ts
+++ src/screens/PetScreen/PetScreen.styles.ts
@@ -3,6 +3,7 @@ import {Dimensions, StyleSheet} from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginBottom:10
   },
   dogImage: {
     width: Dimensions.get('window').width,
